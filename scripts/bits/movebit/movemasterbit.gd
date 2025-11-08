36a94e4147e97984fd2e179daf33983976822bef
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2025-11-08 15:22:59 -0600

    v0.2 - Values
    - Added dynamic Values

--- scripts/bits/movebit/movemasterbit.gd
+++ scripts/bits/movebit/movemasterbit.gd
@@ -1,20 +1,25 @@
 class_name MoveMasterBit extends Bit
 ## Manages all the movement of a Bot; takes MoveBits as children for functionality.
 
+@export var pa:PackedSceneValue
+
 @export var initial_state:MoveBit
 var current_state:MoveBit
 
 ## Changes the current state to the one specified.
 func change_state(to:MoveBit):
-	current_state._on_inactive()
-	current_state.recursive_call("_on_inactive")
+	if current_state != null:
+		current_state._on_inactive()
+		current_state.recursive_call("_on_inactive")
 	
+	print("changed current to ", current_state)
 	current_state = to
 	
-	current_state._on_ictive()
-	current_state.recursive_call("_on_active")
+	if current_state != null:
+		current_state._on_inactive()
+		current_state.recursive_call("_on_active")
 
-@onready var mover = get_mover()
+@onready var mover:CharacterBody2D = get_mover()
 func get_mover() -> CharacterBody2D:
 	
 	var me = self
@@ -33,9 +38,24 @@ func get_move_bits() -> Array[MoveBit]:
 	
 	for child in get_children():
 		if child is MoveBit:
+			child.mover = mover
+			child.master = self
+			
 			response.append(child)
 	
 	return response
+
+func _ready() -> void:
+	print("init: ", initial_state)
+	print("bits: ", get_move_bits())
+	# If no initial state given, default to the first childed one.
+	if initial_state == null and len(movebits) > 0:
+		initial_state = movebits[0]
+	
+	# If there is an initial state, make it the current state.
+	print("init: ", initial_state)
+	if initial_state != null:
+		change_state(initial_state)
 
 func _process(delta: float) -> void:
 	for bit in movebits:
@@ -45,3 +65,22 @@ func _process(delta: float) -> void:
 		else:
 			bit._inactive(delta)
 			bit.recursive_call("_inactive")
+
+func _physics_process(delta: float) -> void:
+	for bit in movebits:
+		if bit == current_state:
+			print("running ", bit)
+			bit._physics_active(delta)
+			bit.recursive_call("_physics_active")
+		else:
+			bit._physics_inactive(delta)
+			bit.recursive_call("_physics_inactive")
+	
+	if mover != null:
+		mover.move_and_slide()
+		
+		if bot is Node2D:
+			bot.global_position += mover.position
+			bot.position = Vector2.ZERO
+	
+	
