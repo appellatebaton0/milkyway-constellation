e7b2bc228e67a062d329ff6df722b110e5145d64
4_Square <104998118+8-Square@users.noreply.github.com>
2025-11-16 15:25:46 +1000

    v0.4.4 - AddBit and DeleteBit Housekeeping
    Includes Houskeeping (To my extent)
    
    Also reverts commit (Due to commit title): 79fa05563947b994620b05e80c16e38cb5d282e3.

--- scripts/bits/addbit.gd
+++ scripts/bits/addbit.gd
@@ -1,33 +1,27 @@
 class_name AddBit extends Bit
 
-# Takes in a NodeValue and PackedScene Value
 @export var parent: NodeValue
 @export var child: PackedSceneValue
 
-#Function that makes an instance of the PackedScene and adds it as a child to the NodeValue
 func AddBitToParent():
 	
-	# Getting the Parent Node
 	var big_node: Node = parent.value()
-	# Checks if Valid #1
+	
 	if not is_instance_valid(big_node):
 		print("Parent Node Incorrect")
 		return 
 	
-	# Getting the Child Node
 	var small_scene: PackedScene = child.value()
-	# Checks if Valid #2
+	
 	if not is_instance_valid(small_scene):
 		print("Packed Scene Incorrect")
 		return
 	
-	# Makes the Instance
 	var add_child_scene: Node = small_scene.instantiate()
-	# Checks if Valid #2
+	
 	if not is_instance_valid(add_child_scene): 
 		print("Packed Scene Instantiate is not working properly")
 		return
 	
-	# Adds the Instance as a child of the Parent Node
 	big_node.add_child(add_child_scene)
-	#print("AddBit Working!")
+	print("AddBit Working!")
